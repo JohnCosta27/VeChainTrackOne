e13c9dd78d4929035a7e041f321256e8e0c52c16
John Costa <johncosta027@gmail.com>
2024-06-29 14:21:32 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  WalletButton,
+  useWallet,
+  useWalletModal,
+} from "@vechain/dapp-kit-react";
+import { useEffect, useState } from "react";
+
+export const App = () => {
+  const { account } = useWallet();
+  const { open, onConnectionStatusChange } = useWalletModal();
+  const [buttonText, setButtonText] = useState("Connect Custom Button");
+
+  useEffect(() => {
+    const handleConnected = (address: string | null) => {
+      if (address) {
+        const formattedAddress = `${address.slice(
+          0,
+          6,
+        )}...${address.slice(-4)}`;
+        setButtonText(`Disconnect from ${formattedAddress}`);
+      } else {
+        setButtonText("Connect Custom Button");
+      }
+    };
+
+    handleConnected(account);
+
+    onConnectionStatusChange(handleConnected);
+  }, [account, onConnectionStatusChange]);
+
+  return (
+    <div className="container">
+      <h2>React JS</h2>
+      <div className="label">kit button:</div>
+      <WalletButton />
+      <div className="label">custom button:</div>
+      <button onClick={open}>{buttonText}</button>
+    </div>
+  );
+};
---
+++ src/main.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { StrictMode } from "react";
+import "./index.css";
+import { App } from "./App";
+import { createRoot } from "react-dom/client";
+import { WalletConnectOptions } from "@vechain/dapp-kit";
+import { DAppKitProvider } from "@vechain/dapp-kit-react";
+
+const walletConnectOptions: WalletConnectOptions = {
+  projectId: "a0b855ceaf109dbc8426479a4c3d38d8",
+  metadata: {
+    name: "Sample VeChain dApp",
+    description: "A sample VeChain dApp",
+    url: window.location.origin,
+    icons: [`${window.location.origin}/images/logo/my-dapp.png`],
+  },
+};
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <DAppKitProvider
+      nodeUrl={"https://testnet.vechain.org/"}
+      genesis={"test"}
+      usePersistence
+      walletConnectOptions={walletConnectOptions}
+    >
+      <App />
+    </DAppKitProvider>
+  </StrictMode>,
+);
