da8c997384416bf1dd6f75616a79a0b8e295309c
John Costa <johncosta027@gmail.com>
2024-06-29 17:53:01 -0700

    flynn is a wanker

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import {
   WalletButton,
+  useConnex,
   useWallet,
   useWalletModal,
 } from "@vechain/dapp-kit-react";
 import { useEffect, useState } from "react";
 
+const CONTRACT_ADDRESS = "0x65DAf03c74E62fB3561365C3482F92B94244f562";
+
+const contractABI = {
+  inputs: [],
+  name: "withdraw",
+  outputs: [],
+  stateMutability: "nonpayable",
+  type: "function",
+};
+
 export const App = () => {
   const { account } = useWallet();
+  const connex = useConnex();
   const { open, onConnectionStatusChange } = useWalletModal();
   const [buttonText, setButtonText] = useState("Connect Custom Button");
 
@@ -29,6 +41,19 @@ export const App = () => {
     onConnectionStatusChange(handleConnected);
   }, [account, onConnectionStatusChange]);
 
+  const callMethod = async () => {
+    const clause = connex.thor
+      .account(CONTRACT_ADDRESS)
+      .method(contractABI)
+      .asClause();
+
+    // making the transaction
+    const result = await connex.vendor
+      .sign("tx", [clause])
+      .comment("calling the store function")
+      .request();
+  };
+
   return (
     <div className="container">
       <h2>React JS</h2>
@@ -36,6 +61,7 @@ export const App = () => {
       <WalletButton />
       <div className="label">custom button:</div>
       <button onClick={open}>{buttonText}</button>
+      <button onClick={callMethod}>fuck around and find out.</button>
     </div>
   );
 };
